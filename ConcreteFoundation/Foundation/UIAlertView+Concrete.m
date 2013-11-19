e0b1861182657c660ca96c80110f63125a58df54
Joshua Dudley <josh@concreteinteractive.com>
2013-11-18 21:42:40 -0800

    Fixed a bug in UIAlertView+Concrete that was causing a crash

--- ConcreteFoundation/Foundation/UIAlertView+Concrete.m
+++ ConcreteFoundation/Foundation/UIAlertView+Concrete.m
@@ -15,12 +15,17 @@
 
 - (id)initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate userInfo:(NSDictionary *)userInfo cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...
 {
-    va_list ap;
-    va_start(ap, otherButtonTitles);
-    self = [self initWithTitle:title message:message delegate:delegate cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles, ap, nil];
-    va_end(ap);
+    self = [self initWithTitle:title message:message delegate:delegate cancelButtonTitle:cancelButtonTitle otherButtonTitles:nil];
     if (self)
     {
+        va_list argumentList;
+        va_start(argumentList, otherButtonTitles);
+        NSString* buttonTitle = nil;
+        while((buttonTitle = va_arg(argumentList, NSString *)))
+        {
+            [self addButtonWithTitle:buttonTitle];
+        }
+        va_end(argumentList);
         self.userInfo = userInfo;
     }
     return self;
@@ -28,18 +33,13 @@
 
 - (NSDictionary *)userInfo
 {
-    @synchronized(NSStringFromClass([self class])) {
-        if (objc_getAssociatedObject(self, USER_INFO_KEY) == nil) {
-            [self setUserInfo:[[[self class] alloc] init]];
-        }
-        return objc_getAssociatedObject(self, USER_INFO_KEY);
-    }
+    return objc_getAssociatedObject(self, USER_INFO_KEY);
 }
 
 - (void)setUserInfo:(NSDictionary *)userInfo
 {
     // OBJC_ASSOCIATION_RETAIN Specifies a strong reference to the associated object, and that the association is made atomically.
-    objc_setAssociatedObject(self, USER_INFO_KEY, userInfo, OBJC_ASSOCIATION_RETAIN);
+    objc_setAssociatedObject(self, USER_INFO_KEY, userInfo, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 @end
